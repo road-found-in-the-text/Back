04b20f760e89c601ce40b99766f928473e2194e7
myhyun0002 <a01089417552@gmail.com>
2023-05-10 12:48:04 +0900

    TR-17 [feat] swagger version change 및 평가 질문 entity 생성

--- src/main/java/com/example/Back/config/SwaggerConfig.java
+++ src/main/java/com/example/Back/config/SwaggerConfig.java
@@ -1,35 +1,22 @@
 package com.example.Back.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "road on the text ",
+                description = "API 명세서",
+                version = "v1"
+        )
+)
 
 @Configuration
-@EnableSwagger2WebMvc
 public class SwaggerConfig {
 
-    @Bean
-    public Docket docket() {
-        return new Docket(DocumentationType.OAS_30)
-                .useDefaultResponseMessages(false)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.Back"))
-                .paths(PathSelectors.any())
-                .build();
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Web Application Api")
-                .description("설명 부분")
-                .version("1.0.0")
-                .build();
-    }
 }
--- src/main/java/com/example/Back/domain/AssessmentQuestion.java
+++ src/main/java/com/example/Back/domain/AssessmentQuestion.java
@@ -1,4 +1,26 @@
 package com.example.Back.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.Where;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "assessment_question")
 public class AssessmentQuestion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="question_id")
+    private Long id;
+
+    private Integer sequence;
+    private Integer sub_sequence;
+    private String question_name;
+
+
 }
