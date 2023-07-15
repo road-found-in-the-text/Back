905da1fc395d626d00b10b62d446e3cab6e32190
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2023-07-15 11:43:10 +0900

    TEXTROAD-6 [fix] cors 에러 해결

--- src/main/java/com/example/Back/config/SecurityConfig.java
+++ src/main/java/com/example/Back/config/SecurityConfig.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
